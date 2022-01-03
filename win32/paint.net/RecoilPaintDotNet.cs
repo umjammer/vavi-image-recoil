979566c5cb4d8d01ba1968ecceb63d414cab2231
Piotr Fusik <fox@scene.pl>
2022-01-03 13:21:47 +0100

    [doc] Bump the copyright year.

--- win32/paint.net/RecoilPaintDotNet.cs
+++ win32/paint.net/RecoilPaintDotNet.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * RecoilPaintDotNet.cs - Paint.NET file type plugin
  *
- * Copyright (C) 2013-2021  Piotr Fusik
+ * Copyright (C) 2013-2022  Piotr Fusik
  *
  * This file is part of RECOIL (Retro Computer Image Library),
  * see http://recoil.sourceforge.net
@@ -32,7 +32,7 @@ using Recoil;
 //[assembly: AssemblyDescription("Decodes images in native formats of vintage computers")]
 [assembly: AssemblyCompany("Piotr Fusik")]
 [assembly: AssemblyProduct("RECOIL")]
-[assembly: AssemblyCopyright("Copyright © 2013-2021")]
+[assembly: AssemblyCopyright("Copyright © 2013-2022")]
 [assembly: AssemblyVersion(RECOIL.Version + ".0")]
 [assembly: AssemblyFileVersion(RECOIL.Version + ".0")]
 
